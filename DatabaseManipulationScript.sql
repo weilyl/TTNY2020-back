12c83747579165574383a0cd8ab9c681710b7aa5
shazena <62306982+shazena@users.noreply.github.com>
2020-12-05 18:52:04 -0500

    Add DML to easily add Sample data to Database

---
+++ DatabaseManipulationScript.sql
@@ -0,0 +1,34 @@
+INSERT INTO `w24ndqvhdse3h5xv`.`user` (`UID`, `username`) VALUES ('123456', 'Smoke');
+INSERT INTO `w24ndqvhdse3h5xv`.`user` (`UID`, `username`) VALUES ('567890', 'Sam');
+INSERT INTO `w24ndqvhdse3h5xv`.`user` (`UID`, `username`) VALUES ('469033', 'Schuyler');
+INSERT INTO `w24ndqvhdse3h5xv`.`user` (`UID`, `username`) VALUES ('892733', 'Siobhan');
+
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('1', 'Aliens Invade the USA');
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('2', 'Kublai Khan Invades');
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('3', 'UFO by Location');
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('4', 'Favorite Food');
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('5', 'Project Blue Book');
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('6', 'Mongols are Invading');
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('7', 'British are Invading');
+INSERT INTO `w24ndqvhdse3h5xv`.`xcard` (`xCardId`, `text`) VALUES ('8', 'French are Invading');
+
+INSERT INTO `w24ndqvhdse3h5xv`.`ycard` (`yCardId`, `text`) VALUES ('1', 'Elephant');
+INSERT INTO `w24ndqvhdse3h5xv`.`ycard` (`yCardId`, `text`) VALUES ('2', 'Cats');
+INSERT INTO `w24ndqvhdse3h5xv`.`ycard` (`yCardId`, `text`) VALUES ('3', 'Potato');
+INSERT INTO `w24ndqvhdse3h5xv`.`ycard` (`yCardId`, `text`) VALUES ('4', 'Lion');
+INSERT INTO `w24ndqvhdse3h5xv`.`ycard` (`yCardId`, `text`) VALUES ('5', 'Ostrich');
+INSERT INTO `w24ndqvhdse3h5xv`.`ycard` (`yCardId`, `text`) VALUES ('6', 'OompaLoompa');
+INSERT INTO `w24ndqvhdse3h5xv`.`ycard` (`yCardId`, `text`) VALUES ('7', 'Tiger');
+
+INSERT INTO `w24ndqvhdse3h5xv`.`game` (`roundsCompleted`, `gameWinner`, `nextJudge`, `entrycode`) VALUES ('3', NULL, '123456', '123ABC');
+INSERT INTO `w24ndqvhdse3h5xv`.`game` (`roundsCompleted`, `gameWinner`, `nextJudge`, `entrycode`) VALUES ('2', NULL, '567890', 'RESPCT');
+
+INSERT INTO `w24ndqvhdse3h5xv`.`round` (`gameId`, `roundWinner`, `roundXCard`, `roundYCard`) VALUES ('1', '123456', '1', '3');
+INSERT INTO `w24ndqvhdse3h5xv`.`round` (`gameId`, `roundWinner`, `roundXCard`, `roundYCard`) VALUES ('1', '469033', '2', '4');
+INSERT INTO `w24ndqvhdse3h5xv`.`round` (`gameId`, `roundWinner`, `roundXCard`, `roundYCard`) VALUES ('1', '892733', '3', '2');
+INSERT INTO `w24ndqvhdse3h5xv`.`round` (`gameId`, `roundWinner`, `roundXCard`, `roundYCard`) VALUES ('2', '469033', '4', '5');
+INSERT INTO `w24ndqvhdse3h5xv`.`round` (`gameId`, `roundWinner`, `roundXCard`, `roundYCard`) VALUES ('2', '123456', '5', '7');
+
+
+
+
