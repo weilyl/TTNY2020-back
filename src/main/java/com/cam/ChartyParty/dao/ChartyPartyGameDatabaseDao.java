02e5892b72ad41807488261883fd4ff27f9079f6
shazena <62306982+shazena@users.noreply.github.com>
2020-12-06 12:29:26 -0500

    Update GameDao to insert time into DB

--- src/main/java/com/cam/ChartyParty/dao/ChartyPartyGameDatabaseDao.java
+++ src/main/java/com/cam/ChartyParty/dao/ChartyPartyGameDatabaseDao.java
@@ -26,7 +26,7 @@ public class ChartyPartyGameDatabaseDao implements ChartyPartyGameDao {
 
     @Override
     public Game add(Game game) {
-        final String sql = "INSERT INTO game(entrycode, roundsCompleted, gameWinner, nextJudge) VALUES(?,?,?,?);";
+        final String sql = "INSERT INTO game(entrycode, roundsCompleted, gameWinner, nextJudge, startTime) VALUES(?,?,?,?,?);";
 
         GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
 
@@ -40,6 +40,7 @@ public class ChartyPartyGameDatabaseDao implements ChartyPartyGameDao {
             statement.setInt(2, game.getRoundscompleted());
             statement.setString(3, game.getWinneruserid());
             statement.setString(4, game.getNextjudgeid());
+            statement.setLong(5, game.getStartTime());
             return statement;
 
         }, keyHolder);
@@ -48,7 +49,6 @@ public class ChartyPartyGameDatabaseDao implements ChartyPartyGameDao {
 
         return game;
     }
-
 
     @Override
     public Game findByEntryCode(String entryCode) throws DataNotFoundException {
@@ -84,30 +84,28 @@ public class ChartyPartyGameDatabaseDao implements ChartyPartyGameDao {
     @Override
     public void deleteById(int id) {
         final String Delete_Round = "DELETE FROM round WHERE gameId = ?;";
-        jdbcTemplate.update(Delete_Round, id) ;
-        
-        
+        jdbcTemplate.update(Delete_Round, id);
+
         final String Delete_Game = "DELETE FROM game WHERE gameId = ?;";
-       jdbcTemplate.update(Delete_Game, id) ;
+        jdbcTemplate.update(Delete_Game, id);
     }
 
     private static final class GameMapper implements RowMapper<Game> {
 
         @Override
         public Game mapRow(ResultSet rs, int index) throws SQLException {
-            String winner=rs.getString("gameWinner");
-            
-            if(winner.isEmpty()){
-                winner="";
+            String winner = rs.getString("gameWinner");
+
+            if (winner.isEmpty()) {
+                winner = "";
             }
-            
+
             Game game = new Game();
             game.setId(rs.getInt("id"));
             game.setEntrycode(rs.getString("entrycode"));
             game.setRoundscompleted(rs.getInt("roundsCompleted"));
             game.setWinneruserid(winner);
             game.setNextjudgeid(rs.getString("nextJudge"));
-           
 
             return game;
         }
